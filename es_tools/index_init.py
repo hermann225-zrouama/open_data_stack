944a8c85832079b1a37156767bd0e6f0adca6aef
ZROUAMA FRANCK HERMANN <debah2002@gmail.com>
2024-06-26 14:43:09 +0000

    new version openstackdata

---
+++ es_tools/index_init.py
@@ -0,0 +1,49 @@
+import requests
+import json
+
+# URL de base de votre cluster Elasticsearch
+base_url = 'http://localhost:9200'  # Remplacez par l'URL de votre cluster
+
+# Nom de l'index que vous souhaitez créer
+index_name = 'taxi_data_index'  # Remplacez par le nom de votre index
+
+# Mapping pour spécifier la structure des données
+index_mapping = {
+    "mappings": {
+        "properties": {
+            "VendorID": {"type": "integer"},
+            "lpep_pickup_datetime": {"type": "date", "format": "yyyy-MM-dd'T'HH:mm:ss"},
+            "lpep_dropoff_datetime": {"type": "date", "format": "yyyy-MM-dd'T'HH:mm:ss"},
+            "store_and_fwd_flag": {"type": "keyword"},
+            "RatecodeID": {"type": "integer"},
+            "PULocationID": {"type": "integer"},
+            "DOLocationID": {"type": "integer"},
+            "passenger_count": {"type": "integer"},
+            "trip_distance": {"type": "float"},
+            "fare_amount": {"type": "float"},
+            "extra": {"type": "float"},
+            "mta_tax": {"type": "float"},
+            "tip_amount": {"type": "float"},
+            "tolls_amount": {"type": "float"},
+            "ehail_fee": {"type": "float"},
+            "improvement_surcharge": {"type": "float"},
+            "total_amount": {"type": "float"},
+            "payment_type": {"type": "integer"},
+            "trip_type": {"type": "integer"},
+            "congestion_surcharge": {"type": "float"}
+        }
+    }
+}
+
+# Fonction pour créer l'index avec le mapping spécifié
+def create_index(index_name, mapping):
+    url = f"{base_url}/{index_name}"
+    headers = {'Content-Type': 'application/json'}
+    response = requests.put(url, headers=headers, json=mapping)
+    return response.json()
+
+# Appel de la fonction pour créer l'index avec mapping
+if __name__ == "__main__":
+    response = create_index(index_name, index_mapping)
+    print(json.dumps(response, indent=2))
+    print(f"Index '{index_name}' créé avec succès.")
